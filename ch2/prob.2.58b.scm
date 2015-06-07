4eb161c1aadaaaa9e838aa778be75fca8a84ea1f
Eric Scrivner <eric.t.scrivner@gmail.com>
2015-06-06 19:16:33 -0700

    More Problem Solutions

--- ch2/prob.2.58b.scm
+++ ch2/prob.2.58b.scm
@@ -1,37 +1,13 @@
 (load "prob.2.58a.scm")
 
-;; The general strategy here derives from the observation that even in infix
-;; form the operators are still binary operators of the from lv op rv. So when
-;; we take the derivative and reduce we simply process three items at a time.
-;; One will the the left-hand value (lv), one will be the operator (op), and
-;; one will be the right-hand value (rv). We might eventually group values with
-;; similar expressions together for reduction. Additionally operator
-;; precendence needs to be taken into account, so the computation can likely
-;; only be performed after a full scan of the expression that rewrites with
-;; correct operator precendence.
-(define example '(3 + 2 + x))
+;; I got tripped up on this problem for a while. It turns out solving the whole
+;; thing is equivalent to solving part (a) and then adding specialized code to
+;; remove the need for parentheses while still respecting the order of
+;; operations.
+(define example '(x + 3 * (x + y + 2)))
 
-(define (make-sum a1 a2)
-  (cond ((=number? a1 0) a2)
-        ((=number? a2 0) a1)
-        ((and (number? a1) (number? a2)) (+ a1 a2))
-        (else
-         (append (if (pair? a1) a1 (list a1))
-                 '(+)
-                 (if (pair? a2) a2 (list a2))))))
+(define (simplify exp)
+  (if (null? (cdr exp)) (car exp) exp))
 
-(define (make-product m1 m2)
-  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
-        ((=number? m1 1) m2)
-        ((=number? m2 1) m1)
-        ((and (number? m1) (number? m2)) (* m1 m2))
-        (else
-         (append (list m1)
-                 '(*)
-                 (list m2)))))
-
-(define (sum? x)
-  (eq? (cadr x) '+))
-
-(define (addend s)
-  (car s))
+(define (augend s) (simplify (cddr s)))
+(define (multiplicand p) (simplify (cddr p)))
---
+++ ch2/prob.2.59.scm
@@ -0,0 +1,26 @@
+;; Theta(n)
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
+
+;; Theta(n)
+(define (adjoin-set x set)
+  (if (element-of-set? x set)
+      set
+      (cons x set)))
+
+;; Theta(n**2)
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+        ((element-of-set? (car set1) set2)
+         (cons (car set1)
+               (intersection-set (cdr set1) set2)))
+        (else (intersection-set (cdr set1) set2))))
+
+;; Theta(n**2)
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+        ((null? set2) set1)
+        (else (adjoin-set (car set1)
+                          (union-set (cdr set1) set2)))))
