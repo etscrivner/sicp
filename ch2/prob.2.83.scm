2be0e2fad328ba40d6a349b242aa0b7ebf6c1c5d
Eric Scrivner <zenogais@gmail.com>
2015-06-20 00:12:30 -0700

    Exercise 2.83 - Actual Type Hierarchy Raising

--- ch2/prob.2.83.scm
+++ ch2/prob.2.83.scm
@@ -1,11 +1,14 @@
 ;; I'm going to skip installing in packages so we can actually test.
-(load "prob.2.1.scm")
+(load "generic-operations.scm")
 
 (define (raise x)
-  (cond ((integer? x) (/ x 1))
-        ((rational? x) x)
-        ((real? x) (make-rectangular x 1))
-        (else (error "Cannot raise type" x))))
+  (let ((proc (get 'raise (type-tag x))))
+    (if proc
+        (proc (contents x))
+        (error "No method for type -- RAISE" x))))
+
+(put 'raise '(scheme-number) (lambda (x) (make-rational x 1)))
+(put 'raise '(rational) (lambda (x) (make-complex-from-real-imag (/ (car x) (cdr x)) 0)))
 
 ;; To actually implement we'd just given rational and real packages their
 ;; respective 'raise methods from the next lowest type.
