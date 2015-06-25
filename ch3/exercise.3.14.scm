cd4a105d2d06d7fad7abf7bb354c898141f2af91
Eric Scrivner <zenogais@gmail.com>
2015-06-25 09:26:57 -0700

    Exercise 3.14

---
+++ ch3/exercise.3.14.scm
@@ -0,0 +1,9 @@
+;; Reverses the list by recursively reversing the order of pointers
+(define (mystery x)
+  (define (loop x y)
+    (if (null? x)
+        y
+        (let ((temp (cdr x)))
+          (set-cdr! x y)
+          (loop temp x))))
+  (loop x '()))
