730b7db9cbf5ebe5804cbe06f651f8496c1fa5cd
Eric Scrivner <eric.t.scrivner@gmail.com>
2015-06-27 06:55:05 -0700

    Exercise 3.18

---
+++ ch3/exercise.3.18.scm
@@ -0,0 +1,11 @@
+(load "exercise.3.13.scm")
+
+(define (has-cycle? x)
+  (define (iter x already-seen)
+    (cond ((not (pair? x)) #f)
+          ((memq x already-seen) #t)
+          (else (iter (cdr x) (cons x already-seen)))))
+  (iter x '()))
+
+(has-cycle? '(a b c))
+(has-cycle? (make-cycle '(a b c)))
