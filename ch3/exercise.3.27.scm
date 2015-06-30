ba2abbf1e7f2413a73f9f6b366dab87a67e047d5
Eric Scrivner <eric.t.scrivner@gmail.com>
2015-06-30 09:52:01 -0700

    Exercise 3.27

---
+++ ch3/exercise.3.27.scm
@@ -0,0 +1,22 @@
+(load "table.scm")
+
+(define (memoize f)
+  (let ((table (make-table)))
+    (lambda (x)
+      (let ((previously-computed-result (lookup x table)))
+        (or previously-computed-result
+            (let ((result (f x)))
+              (insert! x result table)
+              result))))))
+
+(define memo-fib
+  (memoize
+   (lambda (n)
+     (cond ((= n 0) 0)
+           ((= n 1) 1)
+           (else (+ (memo-fib (- n 1))
+                    (memo-fib (- n 2))))))))
+
+
+;;; memo-fib is linear because all fib results are only computed once, after
+;;; that they are O(n) lookup in a table.
