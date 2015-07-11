74009f220ff26e4f9a75abbb673959ca7d2dc9d6
Eric Scrivner <eric.t.scrivner@gmail.com>
2015-07-11 15:19:12 -0700

    Utility Methods/Predicates For Streams

--- ch3/streams.scm
+++ ch3/streams.scm
@@ -33,10 +33,34 @@
 (define (stream-car stream) (car stream))
 (define (stream-cdr stream) (force (cdr stream)))
 
+;; stream methods/predicates
+
+(define (stream-null? s)
+  (null? s))
+
 (define (stream-ref s n)
   (if (= n 0)
       (stream-car s)
       (stream-ref (stream-cdr s) (- n 1))))
+
+(define (stream-map proc s)
+  (if (stream-null? s)
+      the-empty-stream
+      (cons-stream (proc (stream-car s))
+                   (stream-map proc (stream-cdr s)))))
+
+(define (stream-for-each proc s)
+  (if (stream-null? s)
+      'done
+      (begin (proc (stream-car s))
+             (stream-for-each proc (stream-cdr s)))))
+
+(define (display-stream s)
+  (stream-for-each display-line s))
+
+(define (display-line x)
+  (newline x)
+  (display x))
 
 (define (stream-enumerate-interval low high)
   (if (> low high)
@@ -44,3 +68,11 @@
       (cons-stream
        low
        (stream-enumerate-interval (+ low 1) high))))
+
+(define (stream-filter pred stream)
+  (cond ((stream-null? stream) the-empty-stream)
+        ((pred (stream-car stream))
+         (cons-stream (stream-car stream)
+                      (stream-filter pred
+                                     (stream-cdr stream))))
+        (else (stream-filter pred (stream-cdr stream)))))
