8e2a879ea1d49316690e1595873ad57d7c45ca9a
Eric Scrivner <eric.t.scrivner@gmail.com>
2015-07-27 20:17:27 -0700

    Exercise 4.4 - Add Derived Versions

--- ch4/exercise.4.4.scm
+++ ch4/exercise.4.4.scm
@@ -1,16 +1,30 @@
 (load "interpreter.scm")
 
-(define (eval-and exps env)
-  (cond ((last-exp? exps) (true? (eval (first-exp exps) env)))
-        (else (let ((eval-result (eval (first-exp exps) env)))
+(define (eval-and clauses env)
+  (cond ((last-exp? clauses) (true? (eval (first-exp clauses) env)))
+        (else (let ((eval-result (eval (first-exp clauses) env)))
                 (if (true? eval-result)
-                    (eval-and (rest-exps exps) env)
+                    (eval-and (rest-exps clauses) env)
                     eval-result)))))
 
-(define (eval-or exps env)
-  (cond ((last-exp? exps) (true? (eval (first-exp exps) env)))
-        (else (let ((eval-result (eval (first-exp exps) env)))
+(define (eval-or clauses env)
+  (cond ((last-exp? clauses) (true? (eval (first-exp clauses) env)))
+        (else (let ((eval-result (eval (first-exp clauses) env)))
                 (if (true? eval-result)
                     eval-result
-                    (eval-or (rest-exps exps) env))))))
+                    (eval-or (rest-exps clauses) env))))))
 
+
+(define (eval-and-derived clauses env)
+  (if (null? clauses)
+      'false
+      (let ((first (car clauses))
+            (rest (cdr clauses)))
+        (make-if first (eval-and-derived rest env) 'false))))
+
+(define (eval-or-derived clauses env)
+  (if (null? clauses)
+      'false
+      (let ((first (car clauses))
+            (rest (cdr clauses)))
+        (make-if first 'true (eval-or-derived rest env)))))
